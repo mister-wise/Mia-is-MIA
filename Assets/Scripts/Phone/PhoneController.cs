a67543c5dd483e79ac00f5916c0179af003b87d1
Piotr <madry.piotrek@gmail.com>
2023-05-31 01:19:09 +0200

    Recent calls - part 1

--- Assets/Scripts/Phone/PhoneController.cs
+++ Assets/Scripts/Phone/PhoneController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using DG.Tweening;
 using UnityEngine;
@@ -16,6 +17,14 @@ namespace Phone
         [SerializeField] private Transform notificationContainer;
         [SerializeField] private GameObject notificationPrefab;
         [SerializeField] private AudioClip notificationSound;
+        
+        [SerializeField] private Transform recentCallsContainer;
+        [SerializeField] private GameObject recentCallsPrefab;
+        
+        [SerializeField] private GameObject recentCallsWindow;
+
+
+        private List<string> recentCalls = new List<string>();
 
         private int i = 1;
 
@@ -37,15 +46,52 @@ namespace Phone
                 AddNotification(new Notification(NotificationType.Message,"Anonimowy", $"{i} Nie próbuj tego więcej"));
                 i++;
             }
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                AddNotification(new Notification(NotificationType.MissedCall,"Anonimowy", "Missed Call"));
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                CloseRecentCallsWindow();
+            }
         }
+
+        public void BackToHome()
+        {
+            Debug.Log("HOME");
+            CloseRecentCallsWindow();
+        }
+
+        public void OpenRecentCallsWindow()
+        {
+            recentCallsWindow.transform.DOMoveY(0, .5f);
+        }
+        
+        public void CloseRecentCallsWindow()
+        {
+            recentCallsWindow.transform.DOMoveY(-750, .5f);
+        }
+
 
         public void AddNotification(Notification notification)
         {
             var notificationObject = Instantiate(notificationPrefab, notificationContainer);
             notificationObject.GetComponent<NotificationController>()?.SetNotification(notification);
-            notificationObject.transform.DOScale(1, 1);
             audioSource.PlayOneShot(notificationSound);
+
+            if (notification.Type == NotificationType.MissedCall)
+            {
+                AddCallToHistory(notification.Title, RecentCallStatus.Missed);
+            }
         }
+
+        private void AddCallToHistory(string callerName, RecentCallStatus status)
+        {
+            recentCalls.Add(callerName);
+            var recentCallItem = Instantiate(recentCallsPrefab, recentCallsContainer);
+            recentCallItem.GetComponent<RecentCallItem>()?.SetItem(callerName, status);
+        }
+        
         
     }
 }
